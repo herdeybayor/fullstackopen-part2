30e540f36e15b9f537f02906c7c415f73cbb4e55
herdeybayor <shereefdeen4real@gmail.com>
2022-05-15 01:35:11 +0100

    2.13*: Data for countries, step2 done

--- data-for-countries/src/components/Countries.js
+++ data-for-countries/src/components/Countries.js
@@ -1,17 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Countries({ countries }) {
+  const [showIndex, setShowIndex] = useState([]);
+
+  function showToggler(index) {
+    if (showIndex.includes(index)) {
+      return setShowIndex(showIndex.filter((item) => item !== index));
+    }
+    return setShowIndex([...showIndex, index]);
+  }
+
   if (countries.length > 10) {
     return <p>Too many matches, specify another filter</p>;
   }
 
-  if (countries.length !== 1) {
+  if (countries.length !== 1 && countries.length > 1) {
     return (
-      <div>
+      <>
         {countries.map((country, index) => (
-          <p key={index}>{country.name.common}</p>
+          <div key={index}>
+            <p>{country.name.common}</p>
+            <button onClick={() => showToggler(index)}>
+              {!showIndex.includes(index) ? "Show" : "Hide"}
+            </button>
+            {showIndex.includes(index) && (
+              <>
+                <h1>{country.name.common}</h1>
+                <div>
+                  <p>capital {country.capital}</p>
+                  <p>area {country.area}</p>
+                </div>
+                <h3>languages:</h3>
+                <ul>
+                  {Object.keys(country.languages).map((lang, index) => (
+                    <li key={index}>{country.languages[lang]}</li>
+                  ))}
+                </ul>
+                <img src={country.flags.png} alt="" />
+              </>
+            )}
+          </div>
         ))}
-      </div>
+      </>
     );
   }
 
